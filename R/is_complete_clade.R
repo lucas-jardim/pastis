c82bd6daecfeac8e9b0156ae36138a23923e2c62
Gavin Thomas <gavin.thomas@sheffield.ac.uk>
2013-07-23 00:00:00 +0000

    version 0.1-1

--- R/is_complete_clade.R
+++
@@ -1,6 +0,0 @@
-is_complete_clade <- function (set, clades) 
-{
-    present_clades <- get_clades(set, clades)
-    required_taxa <- unlist(clades[present_clades])
-    return(length(intersect(required_taxa, set)) == length(required_taxa))
-}
--- R/reset_constraint_counters.R
+++
@@ -1,5 +0,0 @@
-reset_constraint_counters <- function () 
-{
-    e$constraint_id <- 0
-    e$constraint_class_id <- list()
-}
